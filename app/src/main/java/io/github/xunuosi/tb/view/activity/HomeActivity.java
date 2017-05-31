524acd0d37352b434457b31f4392505c1c2a53a9
xns—_bj <xunuosi@163.com>
2017-05-31 23:52:33 +0800

    添加了Home页布局

--- app/src/main/java/io/github/xunuosi/tb/view/activity/HomeActivity.java
+++ app/src/main/java/io/github/xunuosi/tb/view/activity/HomeActivity.java
@@ -1,11 +1,42 @@
 package io.github.xunuosi.tb.view.activity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.github.xunuosi.tb.R;
 
 public class HomeActivity extends BaseActivity {
 
+    @BindView(R.id.tv_match)
+    TextView tvMatch;
+    @BindView(R.id.tv_street)
+    TextView tvStreet;
+    @BindView(R.id.tv_bull)
+    TextView tvBull;
+
     @Override
     public int setLayoutId() {
-        return R.layout.activity_main;
+        return R.layout.activity_home;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @OnClick({R.id.tv_match, R.id.tv_street, R.id.tv_bull})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_match:
+                break;
+            case R.id.tv_street:
+                break;
+            case R.id.tv_bull:
+                break;
+        }
     }
 }
