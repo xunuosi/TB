0329d8456b8664e5c3aa4bfff6d0019acbd4a63b
xns—_bj <xunuosi@163.com>
2017-06-03 00:02:51 +0800

    修改了球员管理界面的布局

--- app/src/main/java/io/github/xunuosi/tb/view/activity/PlayerManagerActivity.java
+++ app/src/main/java/io/github/xunuosi/tb/view/activity/PlayerManagerActivity.java
@@ -9,6 +9,8 @@ import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.marshalchen.ultimaterecyclerview.UltimateRecyclerView;
@@ -20,6 +22,7 @@ import java.util.List;
 import javax.inject.Inject;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 import io.github.xunuosi.tb.R;
 import io.github.xunuosi.tb.model.bean.Player;
 import io.github.xunuosi.tb.view.adapter.PlayerManagerAdapter;
@@ -29,12 +32,15 @@ import io.github.xunuosi.tb.view.adapter.PlayerManagerAdapter;
  */
 
 public class PlayerManagerActivity extends BaseActivity {
+
     @BindView(R.id.im_back_arrow)
     ImageView mImBackArrow;
     @BindView(R.id.im_add)
     ImageView mImAdd;
-    @BindView(R.id.tool_bar)
-    Toolbar mToolBar;
+    @BindView(R.id.tool_bar_pm)
+    Toolbar toolBar;
+    @BindView(R.id.iv_footer_add)
+    ImageButton ivFooterAdd;
     @BindView(R.id.rv_player_manager)
     UltimateRecyclerView mRvPlayerManager;
 
@@ -43,6 +49,7 @@ public class PlayerManagerActivity extends BaseActivity {
     @Inject
     Context mContext;
     private List<Player> mPlayerList;
+    private int i = 0;
 
     public static Intent getCallIntent(Context context) {
         return new Intent(context, PlayerManagerActivity.class);
@@ -78,18 +85,34 @@ public class PlayerManagerActivity extends BaseActivity {
     @Override
     protected void initData() {
         mPlayerList = new ArrayList<>();
-        for (int i=0;i<10;i++) {
-            Player player = new Player();
-            player.setId(i);
-            player.setName("马克" + i);
-            player.setNum(i);
-            player.setPosition("组织后卫");
-            mPlayerList.add(player);
-        }
+    }
+
+    private void addItem() {
+        Player player = new Player();
+        player.setId(i);
+        player.setName("马克" + i);
+        player.setNum(i);
+        player.setPosition("组织后卫");
+        i++;
+        mAdapter.insert(player, 0);
     }
 
     @Override
     public int setLayoutId() {
         return R.layout.activity_playermanager;
+    }
+
+    @OnClick({R.id.im_back_arrow, R.id.im_add, R.id.iv_footer_add})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.im_back_arrow:
+                break;
+            case R.id.im_add:
+                addItem();
+                break;
+            case R.id.iv_footer_add:
+                addItem();
+                break;
+        }
     }
 }
