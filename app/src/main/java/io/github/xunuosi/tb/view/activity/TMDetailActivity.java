2384d5d4e7555cc8e7941ce28b5c792e85caaa80
xns—_bj <xunuosi@163.com>
2017-06-05 17:54:39 +0800

    Complete Team Managera

--- app/src/main/java/io/github/xunuosi/tb/view/activity/TMDetailActivity.java
+++ app/src/main/java/io/github/xunuosi/tb/view/activity/TMDetailActivity.java
@@ -4,9 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatTextView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.OnClick;
 import io.github.xunuosi.tb.R;
 import io.github.xunuosi.tb.dagger.component.DaggerTMDetailComponent;
 import io.github.xunuosi.tb.dagger.module.TMDetailModule;
@@ -23,6 +29,12 @@ public class TMDetailActivity extends BaseActivity implements ITMDetailView.View
     Context mContext;
     @Inject
     TMDetailPresenter presenter;
+    @BindView(R.id.tv_title)
+    AppCompatTextView tvTitle;
+    @BindView(R.id.iv_item_tm_detail_avator)
+    ImageView ivItemTmDetailAvator;
+    @BindView(R.id.et_team_name)
+    EditText etTeamName;
 
     public static Intent getCallIntent(Context context) {
         return new Intent(context, TMDetailActivity.class);
@@ -46,7 +58,7 @@ public class TMDetailActivity extends BaseActivity implements ITMDetailView.View
 
     @Override
     protected void initViews() {
-
+        tvTitle.setText(R.string.text_team_manager);
     }
 
     @Override
@@ -57,5 +69,32 @@ public class TMDetailActivity extends BaseActivity implements ITMDetailView.View
     @Override
     public int setLayoutId() {
         return R.layout.activity_tm_detail;
+    }
+
+    @OnClick({R.id.im_back_arrow, R.id.im_add, R.id.ib_add_avator, R.id.btn_tm_detail_submit})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.im_back_arrow:
+                break;
+            case R.id.ib_add_avator:
+            break;
+            case R.id.im_add:
+            case R.id.btn_tm_detail_submit:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                break;
+        }
     }
 }
